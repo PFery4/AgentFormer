3e97e853e5f44d9470a37ba3a5be70be9dab9223
Paul Féry <paulfery4@gmail.com>
2023-06-28 12:16:22 +0200

    type hints in preprocessor class

--- data/preprocessor.py
+++ data/preprocessor.py
@@ -1,28 +1,33 @@
 import torch, os, numpy as np, copy
 import cv2
 import glob
+from io import TextIOWrapper
+from typing import Tuple, List, Union
+
 from .map import GeometricMap
+from utils.config import Config
 
 
 class preprocess(object):
     
-    def __init__(self, data_root, seq_name, parser, log, split='train', phase='training'):
-        self.parser = parser
-        self.dataset = parser.dataset
-        self.data_root = data_root
-        self.past_frames = parser.past_frames
-        self.future_frames = parser.future_frames
-        self.frame_skip = parser.get('frame_skip', 1)
-        self.min_past_frames = parser.get('min_past_frames', self.past_frames)
-        self.min_future_frames = parser.get('min_future_frames', self.future_frames)
-        self.traj_scale = parser.traj_scale
-        self.past_traj_scale = parser.traj_scale
-        self.load_map = parser.get('load_map', False)
-        self.map_version = parser.get('map_version', '0.1')
-        self.seq_name = seq_name
-        self.split = split
-        self.phase = phase
-        self.log = log
+    def __init__(self, data_root: str, seq_name: str, parser: Config, log: TextIOWrapper,
+                 split: str = 'train', phase: str = 'training'):
+        self.parser = parser        # Config
+        self.dataset = parser.dataset       # str
+        self.data_root = data_root          # str
+        self.past_frames = parser.past_frames       # int
+        self.future_frames = parser.future_frames   # int
+        self.frame_skip = parser.get('frame_skip', 1)       # int
+        self.min_past_frames = parser.get('min_past_frames', self.past_frames)      # int
+        self.min_future_frames = parser.get('min_future_frames', self.future_frames)        # int
+        self.traj_scale = parser.traj_scale     # int
+        self.past_traj_scale = parser.traj_scale        # int
+        self.load_map = parser.get('load_map', False)       # bool
+        self.map_version = parser.get('map_version', '0.1')     # str
+        self.seq_name = seq_name        # str
+        self.split = split          # str
+        self.phase = phase          # str
+        self.log = log              # TextIOWrapper
 
         if parser.dataset == 'nuscenes_pred':
             label_path = os.path.join(data_root, 'label/{}/{}.txt'.format(split, seq_name))
@@ -33,11 +38,11 @@ class preprocess(object):
         else:
             assert False, 'error'
 
-        self.gt = np.genfromtxt(label_path, delimiter=delimiter, dtype=str)
+        self.gt = np.genfromtxt(label_path, delimiter=delimiter, dtype=str)     # np.ndarray
         frames = self.gt[:, 0].astype(np.float32).astype(int)
         fr_start, fr_end = frames.min(), frames.max()
-        self.init_frame = fr_start
-        self.num_fr = fr_end + 1 - fr_start
+        self.init_frame = fr_start      # np.int64
+        self.num_fr = fr_end + 1 - fr_start     # np.int64
 
         if self.load_map:
             self.load_scene_map()
@@ -46,38 +51,38 @@ class preprocess(object):
 
         self.class_names = class_names = {'Pedestrian': 1, 'Car': 2, 'Cyclist': 3, 'Truck': 4, 'Van': 5, 'Tram': 6, 'Person': 7, \
             'Misc': 8, 'DontCare': 9, 'Traffic_cone': 10, 'Construction_vehicle': 11, 'Barrier': 12, 'Motorcycle': 13, \
-            'Bicycle': 14, 'Bus': 15, 'Trailer': 16, 'Emergency': 17, 'Construction': 18}
+            'Bicycle': 14, 'Bus': 15, 'Trailer': 16, 'Emergency': 17, 'Construction': 18}       # dict
         for row_index in range(len(self.gt)):
             self.gt[row_index][2] = class_names[self.gt[row_index][2]]
-        self.gt = self.gt.astype('float32')
-        self.xind, self.zind = 13, 15
+        self.gt = self.gt.astype('float32')     # np.ndarray
+        self.xind, self.zind = 13, 15           # int int
 
-    def GetID(self, data):
+    def GetID(self, data: np.ndarray) -> List[np.float32]:
         id = []
         for i in range(data.shape[0]):
             id.append(data[i, 1].copy())
         return id
 
-    def TotalFrame(self):
+    def TotalFrame(self) -> np.int64:
         return self.num_fr
 
-    def PreData(self, frame):
+    def PreData(self, frame: np.int64) -> List[np.ndarray]:
         DataList = []
         for i in range(self.past_frames):
             if frame - i < self.init_frame:              
                 data = []
-            data = self.gt[self.gt[:, 0] == (frame - i * self.frame_skip)]    
+            data = self.gt[self.gt[:, 0] == (frame - i * self.frame_skip)]
             DataList.append(data)
         return DataList
     
-    def FutureData(self, frame):
+    def FutureData(self, frame: np.int64) -> List[np.ndarray]:
         DataList = []
         for i in range(1, self.future_frames + 1):
             data = self.gt[self.gt[:, 0] == (frame + i * self.frame_skip)]
             DataList.append(data)
         return DataList
 
-    def get_valid_id(self, pre_data, fut_data):
+    def get_valid_id(self, pre_data: List[np.ndarray], fut_data: List[np.ndarray]) -> List[np.float32]:
         cur_id = self.GetID(pre_data[0])
         valid_id = []
         for idx in cur_id:
@@ -87,19 +92,19 @@ class preprocess(object):
                 valid_id.append(idx)
         return valid_id
 
-    def get_pred_mask(self, cur_data, valid_id):
+    def get_pred_mask(self, cur_data: np.ndarray, valid_id: List[np.float32]) -> np.ndarray:
         pred_mask = np.zeros(len(valid_id), dtype=int)
         for i, idx in enumerate(valid_id):
             pred_mask[i] = cur_data[cur_data[:, 1] == idx].squeeze()[-1]
         return pred_mask
 
-    def get_heading(self, cur_data, valid_id):
+    def get_heading(self, cur_data: np.ndarray, valid_id: List[np.float32]) -> np.ndarray:
         heading = np.zeros(len(valid_id))
         for i, idx in enumerate(valid_id):
             heading[i] = cur_data[cur_data[:, 1] == idx].squeeze()[16]
         return heading
 
-    def load_scene_map(self):
+    def load_scene_map(self) -> None:
         map_file = f'{self.data_root}/map_{self.map_version}/{self.seq_name}.png'
         map_vis_file = f'{self.data_root}/map_{self.map_version}/vis_{self.seq_name}.png'
         map_meta_file = f'{self.data_root}/map_{self.map_version}/meta_{self.seq_name}.txt'
@@ -112,7 +117,8 @@ class preprocess(object):
         self.geom_scene_map = GeometricMap(self.scene_map, homography, self.map_origin)
         self.scene_vis_map = GeometricMap(self.scene_vis_map, homography, self.map_origin)
 
-    def PreMotion(self, DataTuple, valid_id):
+    def PreMotion(self, DataTuple: List[np.ndarray], valid_id: List[np.float32])\
+            -> Tuple[List[torch.Tensor], List[torch.Tensor]]:
         motion = []
         mask = []
         for identity in valid_id:
@@ -132,7 +138,8 @@ class preprocess(object):
             mask.append(mask_i)
         return motion, mask
 
-    def FutureMotion(self, DataTuple, valid_id):
+    def FutureMotion(self, DataTuple: List[np.ndarray], valid_id: List[np.float32])\
+            -> Tuple[List[torch.Tensor], List[torch.Tensor]]:
         motion = []
         mask = []
         for identity in valid_id:
@@ -152,14 +159,15 @@ class preprocess(object):
             mask.append(mask_i)
         return motion, mask
 
-    def __call__(self, frame):
+    def __call__(self, frame: np.int64) -> Union[dict, None]:
 
-        assert frame - self.init_frame >= 0 and frame - self.init_frame <= self.TotalFrame() - 1, 'frame is %d, total is %d' % (frame, self.TotalFrame())
+        assert frame - self.init_frame >= 0 and frame - self.init_frame <= self.TotalFrame() - 1,\
+            'frame is %d, total is %d' % (frame, self.TotalFrame())
 
-    
         pre_data = self.PreData(frame)
         fut_data = self.FutureData(frame)
         valid_id = self.get_valid_id(pre_data, fut_data)
+
         if len(pre_data[0]) == 0 or len(fut_data[0]) == 0 or len(valid_id) == 0:
             return None
 
