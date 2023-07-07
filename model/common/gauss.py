13efd6003d4123557e4cabb15b89f8008c633c19
Paul Féry <paulfery4@gmail.com>
2023-07-07 12:27:43 +0200

    v2 of nll function (more computationally efficient)

--- model/common/gauss.py
+++ model/common/gauss.py
@@ -15,7 +15,7 @@ class Gaussian_Density_Model(nn.Module):
         super().__init__()
         assert activation in ("tanh", "sigmoid", "relu"), ValueError(f"activation type unknown: {activation}")
 
-        self.activation = getattr(torch, activation)        # default: torch.relu
+        self.activation = getattr(torch, activation)  # default: torch.relu
 
         layer_dims = [input_dim, *hidden_dims]
 
@@ -48,13 +48,14 @@ if __name__ == '__main__':
 
     from torch.distributions import MultivariateNormal
     from tqdm import tqdm
+    from time import time
 
     dist_params = {
         "mu": [0., 0.],
         "sig": [1., 1.],
         "rho": 0.0
     }
-    tensor_shape = [30, 12]     # epochs, batches, datablobs...
+    tensor_shape = [30, 12]  # epochs, batches, datablobs...
     model_input_dim = 256
     model_hidden_dims = [128, 64]
     model_activation = "relu"
@@ -69,16 +70,16 @@ if __name__ == '__main__':
         rho.shape -> (*, 1)
         targets.shape -> (*, 2)
         """
-        mux, muy = mu[..., 0].unsqueeze(-1), mu[..., 1].unsqueeze(-1)       # shape (*, 1)
-        sigx, sigy = sig[..., 0].unsqueeze(-1), sig[..., 1].unsqueeze(-1)   # shape (*, 1)
-        gt_x, gt_y = targets[..., 0].unsqueeze(-1), targets[..., 1].unsqueeze(-1)       # shape (*, 1)
+        mux, muy = mu[..., 0].unsqueeze(-1), mu[..., 1].unsqueeze(-1)  # shape (*, 1)
+        sigx, sigy = sig[..., 0].unsqueeze(-1), sig[..., 1].unsqueeze(-1)  # shape (*, 1)
+        gt_x, gt_y = targets[..., 0].unsqueeze(-1), targets[..., 1].unsqueeze(-1)  # shape (*, 1)
 
         norm_x, norm_y = (gt_x - mux) / sigx, (gt_y - muy) / sigy
 
-        z = norm_x**2 + norm_y**2 - 2 * rho * norm_x * norm_y
-        min_r = 1 - rho**2
+        z = norm_x ** 2 + norm_y ** 2 - 2 * rho * norm_x * norm_y
+        min_r = 1 - rho ** 2
 
-        result = torch.exp(-z/(2 * min_r))
+        result = torch.exp(-z / (2 * min_r))
         denom = 2 * np.pi * sigx * sigy * torch.sqrt(min_r)
 
         result = result / denom
@@ -87,9 +88,38 @@ if __name__ == '__main__':
 
         return torch.sum(result)
 
+
+    def gaussian_twodee_nll_2(mu, sig, rho, targets, eps: float = 1e-20):
+        """
+        referring to : https://stats.stackexchange.com/questions/521091/optimizing-gaussian-negative-log-likelihood
+        mu.shape -> (*, 2)
+        sig.shape -> (*, 2)
+        rho.shape -> (*, 1)
+        targets.shape -> (*, 2)
+        """
+        mux, muy = mu[..., 0].unsqueeze(-1), mu[..., 1].unsqueeze(-1)  # shape (*, 1)
+        sigx, sigy = sig[..., 0].unsqueeze(-1), sig[..., 1].unsqueeze(-1)  # shape (*, 1)
+        gt_x, gt_y = targets[..., 0].unsqueeze(-1), targets[..., 1].unsqueeze(-1)  # shape (*, 1)
+
+        sigx_sq, sigy_sq = sigx ** 2, sigy ** 2
+        min_r = 1 - rho ** 2
+        normx, normy = (gt_x - mux) / torch.clamp(sigx, min=eps), (gt_y - muy) / torch.clamp(sigy, min=eps)
+
+        # const = torch.ones_like(rho) * 4 * np.pi ** 2
+
+        result = torch.log(torch.clamp(sigx_sq, min=eps)) + \
+                 torch.log(torch.clamp(sigy_sq, min=eps)) + \
+                 torch.log(torch.clamp(min_r, min=eps)) + \
+                 (normx ** 2 + normy ** 2 - 2 * rho * normx * normy) / min_r    # + torch.log(const)
+
+        return 0.5 * torch.sum(result)
+
+
     gt_mu = torch.tensor(dist_params["mu"], dtype=torch.float64)
-    gt_Sig = torch.tensor([[dist_params["sig"][0]**2, dist_params["rho"] * dist_params["sig"][0] * dist_params["sig"][1]],
-                        [dist_params["rho"] * dist_params["sig"][0] * dist_params["sig"][1], dist_params["sig"][1]**2]], dtype=torch.float64)
+    gt_Sig = torch.tensor(
+        [[dist_params["sig"][0] ** 2, dist_params["rho"] * dist_params["sig"][0] * dist_params["sig"][1]],
+         [dist_params["rho"] * dist_params["sig"][0] * dist_params["sig"][1], dist_params["sig"][1] ** 2]],
+        dtype=torch.float64)
 
     print(f"{gt_mu=}")
     print(f"{gt_Sig=}")
@@ -109,9 +139,10 @@ if __name__ == '__main__':
         x = torch.randn([*tensor_shape, model_input_dim])
 
         mu, sig, rho = gauss(x)
-        loss = gaussian_twodee_nll(mu, sig, rho, y)
+        loss = gaussian_twodee_nll_2(mu, sig, rho, y)
 
         loss.backward()
+
         optimizer.step()
 
         assert torch.all(sig >= 0.0)
@@ -128,4 +159,3 @@ if __name__ == '__main__':
     print(f"{torch.mean(sig[..., 0])=}")
     print(f"{torch.mean(sig[..., 1])=}")
     print(f"{torch.mean(rho[..., 0])=}")
-
