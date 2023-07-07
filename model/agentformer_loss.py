a74363617148d67af65e2dc970d4c7ebc14e04f2
Paul Féry <paulfery4@gmail.com>
2023-07-07 12:42:12 +0200

    move loss definition to agentformer_loss.py file

--- model/agentformer_loss.py
+++ model/agentformer_loss.py
@@ -1,3 +1,5 @@
+import torch
+from numpy import pi
 
 
 def compute_motion_mse(data, cfg):
@@ -10,6 +12,61 @@ def compute_motion_mse(data, cfg):
         loss_unweighted /= diff.shape[0]
     loss = loss_unweighted * cfg['weight']
     return loss, loss_unweighted
+
+
+def gaussian_twodee_nll(mu, sig, rho, targets, reduction: str = "sum", eps: float = 1e-20):
+    """
+    referring to : https://github.com/quancore/social-lstm/blob/bdddf36e312c9dde7a5f1d447000e59918346520/helper.py#L250
+    mu.shape -> (*, 2)
+    sig.shape -> (*, 2)
+    rho.shape -> (*, 1)
+    targets.shape -> (*, 2)
+    reduction -> ["mean", "sum"]
+    """
+    mux, muy = mu[..., 0].unsqueeze(-1), mu[..., 1].unsqueeze(-1)  # shape (*, 1)
+    sigx, sigy = sig[..., 0].unsqueeze(-1), sig[..., 1].unsqueeze(-1)  # shape (*, 1)
+    gt_x, gt_y = targets[..., 0].unsqueeze(-1), targets[..., 1].unsqueeze(-1)  # shape (*, 1)
+
+    norm_x, norm_y = (gt_x - mux) / sigx, (gt_y - muy) / sigy
+
+    z = norm_x ** 2 + norm_y ** 2 - 2 * rho * norm_x * norm_y
+    min_r = 1 - rho ** 2
+
+    result = torch.exp(-z / (2 * min_r))
+    denom = 2 * pi * sigx * sigy * torch.sqrt(min_r)
+
+    result = result / denom
+
+    result = -torch.log(torch.clamp(result, min=eps))
+
+    return getattr(torch, reduction)(result)
+
+
+def gaussian_twodee_nll_2(mu, sig, rho, targets, reduction: str = "sum", eps: float = 1e-20):
+    """
+    referring to : https://stats.stackexchange.com/questions/521091/optimizing-gaussian-negative-log-likelihood
+    mu.shape -> (*, 2)
+    sig.shape -> (*, 2)
+    rho.shape -> (*, 1)
+    targets.shape -> (*, 2)
+    reduction -> ["mean", "sum"]
+    """
+    mux, muy = mu[..., 0].unsqueeze(-1), mu[..., 1].unsqueeze(-1)  # shape (*, 1)
+    sigx, sigy = sig[..., 0].unsqueeze(-1), sig[..., 1].unsqueeze(-1)  # shape (*, 1)
+    gt_x, gt_y = targets[..., 0].unsqueeze(-1), targets[..., 1].unsqueeze(-1)  # shape (*, 1)
+
+    sigx_sq, sigy_sq = sigx ** 2, sigy ** 2
+    min_r = 1 - rho ** 2
+    normx, normy = (gt_x - mux) / torch.clamp(sigx, min=eps), (gt_y - muy) / torch.clamp(sigy, min=eps)
+
+    # const = torch.ones_like(rho) * 4 * pi ** 2
+
+    result = torch.log(torch.clamp(sigx_sq, min=eps)) + \
+             torch.log(torch.clamp(sigy_sq, min=eps)) + \
+             torch.log(torch.clamp(min_r, min=eps)) + \
+             (normx ** 2 + normy ** 2 - 2 * rho * normx * normy) / min_r    # + torch.log(const)
+
+    return 0.5 * getattr(torch, reduction)(result)
 
 
 def compute_gauss_nll(data, cfg):
--- model/common/gauss.py
+++ model/common/gauss.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torch.nn as nn
 import torch
 from typing import List
@@ -48,7 +47,7 @@ if __name__ == '__main__':
 
     from torch.distributions import MultivariateNormal
     from tqdm import tqdm
-    from time import time
+    from model.agentformer_loss import gaussian_twodee_nll, gaussian_twodee_nll_2
 
     dist_params = {
         "mu": [0., 0.],
@@ -60,60 +59,7 @@ if __name__ == '__main__':
     model_hidden_dims = [128, 64]
     model_activation = "relu"
     n_training_steps = 1000
-
-
-    def gaussian_twodee_nll(mu, sig, rho, targets, eps: float = 1e-20):
-        """
-        referring to : https://github.com/quancore/social-lstm/blob/bdddf36e312c9dde7a5f1d447000e59918346520/helper.py#L250
-        mu.shape -> (*, 2)
-        sig.shape -> (*, 2)
-        rho.shape -> (*, 1)
-        targets.shape -> (*, 2)
-        """
-        mux, muy = mu[..., 0].unsqueeze(-1), mu[..., 1].unsqueeze(-1)  # shape (*, 1)
-        sigx, sigy = sig[..., 0].unsqueeze(-1), sig[..., 1].unsqueeze(-1)  # shape (*, 1)
-        gt_x, gt_y = targets[..., 0].unsqueeze(-1), targets[..., 1].unsqueeze(-1)  # shape (*, 1)
-
-        norm_x, norm_y = (gt_x - mux) / sigx, (gt_y - muy) / sigy
-
-        z = norm_x ** 2 + norm_y ** 2 - 2 * rho * norm_x * norm_y
-        min_r = 1 - rho ** 2
-
-        result = torch.exp(-z / (2 * min_r))
-        denom = 2 * np.pi * sigx * sigy * torch.sqrt(min_r)
-
-        result = result / denom
-
-        result = -torch.log(torch.clamp(result, min=eps))
-
-        return torch.sum(result)
-
-
-    def gaussian_twodee_nll_2(mu, sig, rho, targets, eps: float = 1e-20):
-        """
-        referring to : https://stats.stackexchange.com/questions/521091/optimizing-gaussian-negative-log-likelihood
-        mu.shape -> (*, 2)
-        sig.shape -> (*, 2)
-        rho.shape -> (*, 1)
-        targets.shape -> (*, 2)
-        """
-        mux, muy = mu[..., 0].unsqueeze(-1), mu[..., 1].unsqueeze(-1)  # shape (*, 1)
-        sigx, sigy = sig[..., 0].unsqueeze(-1), sig[..., 1].unsqueeze(-1)  # shape (*, 1)
-        gt_x, gt_y = targets[..., 0].unsqueeze(-1), targets[..., 1].unsqueeze(-1)  # shape (*, 1)
-
-        sigx_sq, sigy_sq = sigx ** 2, sigy ** 2
-        min_r = 1 - rho ** 2
-        normx, normy = (gt_x - mux) / torch.clamp(sigx, min=eps), (gt_y - muy) / torch.clamp(sigy, min=eps)
-
-        # const = torch.ones_like(rho) * 4 * np.pi ** 2
-
-        result = torch.log(torch.clamp(sigx_sq, min=eps)) + \
-                 torch.log(torch.clamp(sigy_sq, min=eps)) + \
-                 torch.log(torch.clamp(min_r, min=eps)) + \
-                 (normx ** 2 + normy ** 2 - 2 * rho * normx * normy) / min_r    # + torch.log(const)
-
-        return 0.5 * torch.sum(result)
-
+    nll_loss = gaussian_twodee_nll_2  # [gaussian_twodee_nll, gaussian_twodee_nll_2]
 
     gt_mu = torch.tensor(dist_params["mu"], dtype=torch.float64)
     gt_Sig = torch.tensor(
@@ -139,7 +85,7 @@ if __name__ == '__main__':
         x = torch.randn([*tensor_shape, model_input_dim])
 
         mu, sig, rho = gauss(x)
-        loss = gaussian_twodee_nll_2(mu, sig, rho, y)
+        loss = nll_loss(mu, sig, rho, y)
 
         loss.backward()
 
