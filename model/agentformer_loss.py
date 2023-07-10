9bcd7e45b27500c187f2608adc141182d67fb70d
Paul Féry <paulfery4@gmail.com>
2023-07-10 17:30:38 +0200

    NLL loss now respects the mask configuration variable

--- model/agentformer_loss.py
+++ model/agentformer_loss.py
@@ -14,14 +14,14 @@ def compute_motion_mse(data, cfg):
     return loss, loss_unweighted
 
 
-def gaussian_twodee_nll(mu, sig, rho, targets, reduction: str = "mean", eps: float = 1e-20):
+def gaussian_twodee_nll(mu, sig, rho, targets, eps: float = 1e-20):
     """
     referring to : https://github.com/quancore/social-lstm/blob/bdddf36e312c9dde7a5f1d447000e59918346520/helper.py#L250
     mu.shape -> (*, 2)
     sig.shape -> (*, 2)
     rho.shape -> (*, 1)
     targets.shape -> (*, 2)
-    reduction -> ["mean", "sum"]
+    reduction -> ["mean", "sum", "tensor"]
     """
     mux, muy = mu[..., 0].unsqueeze(-1), mu[..., 1].unsqueeze(-1)  # shape (*, 1)
     sigx, sigy = sig[..., 0].unsqueeze(-1), sig[..., 1].unsqueeze(-1)  # shape (*, 1)
@@ -39,17 +39,16 @@ def gaussian_twodee_nll(mu, sig, rho, targets, reduction: str = "mean", eps: flo
 
     result = -torch.log(torch.clamp(result, min=eps))
 
-    return getattr(torch, reduction)(result)
+    return result
 
 
-def gaussian_twodee_nll_2(mu, sig, rho, targets, reduction: str = "mean", eps: float = 1e-20):
+def gaussian_twodee_nll_2(mu, sig, rho, targets, eps: float = 1e-20):
     """
     referring to : https://stats.stackexchange.com/questions/521091/optimizing-gaussian-negative-log-likelihood
     mu.shape -> (*, 2)
     sig.shape -> (*, 2)
     rho.shape -> (*, 1)
     targets.shape -> (*, 2)
-    reduction -> ["mean", "sum"]
     """
     mux, muy = mu[..., 0].unsqueeze(-1), mu[..., 1].unsqueeze(-1)  # shape (*, 1)
     sigx, sigy = sig[..., 0].unsqueeze(-1), sig[..., 1].unsqueeze(-1)  # shape (*, 1)
@@ -66,34 +65,35 @@ def gaussian_twodee_nll_2(mu, sig, rho, targets, reduction: str = "mean", eps: f
              torch.log(torch.clamp(min_r, min=eps)) + \
              (normx ** 2 + normy ** 2 - 2 * rho * normx * normy) / min_r    # + torch.log(const)
 
-    return 0.5 * getattr(torch, reduction)(result)
+    return result
 
 
-def multivariate_gaussian_nll(mu, Sig, targets, reduction: str = "mean", eps: float = 1e-20):
+def multivariate_gaussian_nll(mu, Sig, targets, eps: float = 1e-20):
     """
     mu.shape -> (*, N)
     Sig.shape -> (*, N, N)
     targets.shape -> (*, N)
-    reduction -> ["mean", "sum"]
     """
     norm = (targets - mu).unsqueeze(-1)     # (*, N, 1)
 
-    t1 = norm.transpose(-2, -1) @ torch.linalg.inv(torch.clamp(Sig, min=eps)) @ norm
-    t2 = torch.log(torch.linalg.det(torch.clamp(Sig, min=eps)))
-    # t3 = torch.log(torch.ones(mu.shape[-1]) * (2 * pi)**2)
+    t1 = (norm.transpose(-2, -1) @ torch.linalg.inv(torch.clamp(Sig, min=eps)) @ norm).view(mu.shape[:-1])      # (*)
+    t2 = torch.log(torch.linalg.det(torch.clamp(Sig, min=eps)))                                                 # (*)
+    # t3 = torch.log(torch.sum(torch.ones_like(mu) * (2 * pi)**2, dim=-1))
 
     result = t1 + t2    # + t3
 
-    return 0.5 * getattr(torch, reduction)(result)
+    return result
 
 
 def compute_gauss_nll(data, cfg):
     loss_unweighted = multivariate_gaussian_nll(mu=data['train_dec_mu'],
                                                 Sig=data['train_dec_Sig'],
                                                 targets=data['fut_motion_orig'])
-    # # todo: how to apply mask?
-    # if cfg.get('mask', True):
-    #     mask = data['fut_mask']
+    if cfg.get('mask', True):
+        mask = data['fut_mask']
+        loss_unweighted *= mask
+    loss_unweighted = torch.mean(loss_unweighted) if cfg.get('normalize', True) else torch.sum(loss_unweighted)
+    loss_unweighted = 0.5 * loss_unweighted         # why 0.5? -> https://stats.stackexchange.com/questions/521091/optimizing-gaussian-negative-log-likelihood
     loss = loss_unweighted * cfg['weight']
     return loss, loss_unweighted
 
