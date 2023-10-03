9b32c8effe6381556f3d159342746429625a7c58
Paul Féry <paulfery4@gmail.com>
2023-10-03 14:49:56 +0200

    remove commented out code, begin writing map_agent_aware_attention

--- model/agentformer_lib.py
+++ model/agentformer_lib.py
@@ -59,6 +59,29 @@ def visualize_mask(q_identities: Tensor, k_identities: Tensor) -> None:
     plt.show()
 
 
+def map_agent_aware_attention(
+        map_feature: Tensor,
+) -> None:
+    # tgt_len, batch_size, embed_dim = query.size()       # L, N, E
+    # print(f"{tgt_len, batch_size, embed_dim=}")
+    # assert embed_dim == embed_dim_to_check
+    # # allow MHA to have different sizes for the feature dimension
+    # assert key.size(0) == value.size(0) and key.size(1) == value.size(1)
+    #
+    # head_dim = embed_dim // num_heads
+    # assert head_dim * num_heads == embed_dim, f"embed_dim ({embed_dim}) must be divisible by num_heads ({num_heads})"
+    #
+    # scaling = float(head_dim) ** -0.5
+    #
+    # use_separate_proj_weight = False
+    # if torch.equal(query, key) and torch.equal(key, value):
+    #     # self.attention, each tensor is of shape []
+    #     k = 0
+
+    # TODO: IMPLEMENT WIP WIP WIP
+    pass
+
+
 def agent_aware_attention(query: Tensor,
                           key: Tensor,
                           value: Tensor,
@@ -144,20 +167,12 @@ def agent_aware_attention(query: Tensor,
         - attn_output_weights: :math:`(N, L, S)` where N is the batch size,
           L is the target sequence length, S is the source sequence length.
     """
+    # [print(f"{k}: {v}\n") for k, v in locals().items()]
+    # print(zblu)
+    # print(f"{torch.jit.is_scripting()=}")
     # if not torch.jit.is_scripting():
-    #     tens_ops = (query, key, value, in_proj_weight, in_proj_bias, bias_k, bias_v,
-    #                 out_proj_weight, out_proj_bias)
-    #     if any([type(t) is not Tensor for t in tens_ops]) and has_torch_function(tens_ops):
-    #         # return handle_torch_function(
-    #         #     multi_head_attention_forward, tens_ops, query, key, value,
-    #         #     embed_dim_to_check, num_heads, in_proj_weight, in_proj_bias,
-    #         #     bias_k, bias_v, add_zero_attn, dropout_p, out_proj_weight,
-    #         #     out_proj_bias, training=training, key_padding_mask=key_padding_mask,
-    #         #     need_weights=need_weights, attn_mask=attn_mask,
-    #         #     use_separate_proj_weight=use_separate_proj_weight,
-    #         #     q_proj_weight=q_proj_weight, k_proj_weight=k_proj_weight,
-    #         #     v_proj_weight=v_proj_weight, static_k=static_k, static_v=static_v)
-    #         raise NotImplementedError
+    #     raise NotImplementedError
+
     tgt_len, batch_size, embed_dim = query.size()
     assert embed_dim == embed_dim_to_check
     # allow MHA to have different sizes for the feature dimension
@@ -171,6 +186,11 @@ def agent_aware_attention(query: Tensor,
         if torch.equal(query, key) and torch.equal(key, value):
             # self-attention. each tensor is of shape [tgt_len, embed_dim]
             q, k, v = linear(query, in_proj_weight, in_proj_bias).chunk(3, dim=-1)
+            # WIP CODE
+            print(f"{query.shape, in_proj_weight.shape, in_proj_bias.shape=}")
+            print(f"{q.shape, k.shape, v.shape=}")
+            print(zblu)
+            # WIP CODE
             if in_proj_weight_self is not None:
                 q_self, k_self = linear(query, in_proj_weight_self, in_proj_bias_self).chunk(2, dim=-1)
         elif torch.equal(key, value):
@@ -210,29 +230,7 @@ def agent_aware_attention(query: Tensor,
     # q, k, v and q_self, k_self are of shape [tgt_len, embed_dim]
 
     else:
-        # # this bit of code is likely not used at all, as q_self and k_self are not generated
-        # q_proj_weight_non_opt = torch.jit._unwrap_optional(q_proj_weight)
-        # len1, len2 = q_proj_weight_non_opt.size()
-        # assert len1 == embed_dim and len2 == query.size(-1)
-        #
-        # k_proj_weight_non_opt = torch.jit._unwrap_optional(k_proj_weight)
-        # len1, len2 = k_proj_weight_non_opt.size()
-        # assert len1 == embed_dim and len2 == key.size(-1)
-        #
-        # v_proj_weight_non_opt = torch.jit._unwrap_optional(v_proj_weight)
-        # len1, len2 = v_proj_weight_non_opt.size()
-        # assert len1 == embed_dim and len2 == value.size(-1)
-        #
-        # if in_proj_bias is not None:
-        #     q = linear(query, q_proj_weight_non_opt, in_proj_bias[0:embed_dim])
-        #     k = linear(key, k_proj_weight_non_opt, in_proj_bias[embed_dim:(embed_dim * 2)])
-        #     v = linear(value, v_proj_weight_non_opt, in_proj_bias[(embed_dim * 2):])
-        # else:
-        #     q = linear(query, q_proj_weight_non_opt, in_proj_bias)
-        #     k = linear(key, k_proj_weight_non_opt, in_proj_bias)
-        #     v = linear(value, v_proj_weight_non_opt, in_proj_bias)
-        raise NotImplementedError("NEED TO CHECK FOR PROPER MANIPULATION OF PARTIALLY NAN TENSORS"
-                                  "(ie, NaN patterns between q, k, v and query, key, value need to match)")
+        raise NotImplementedError
 
     # print(f"{gaussian_kernel=}")
     if not gaussian_kernel:
@@ -254,8 +252,6 @@ def agent_aware_attention(query: Tensor,
             if list(attn_mask.size()) != [1, query.size(0), key.size(0)]:
                 raise RuntimeError('The size of the 2D attn_mask is not correct.')
         elif attn_mask.dim() == 3:
-            # if list(attn_mask.size()) != [batch_size * num_heads, query.size(0), key.size(0)]:
-            #     raise RuntimeError('The size of the 3D attn_mask is not correct.')
             raise RuntimeError('The size of the 3D attn_mask is not correct.')
         else:
             raise RuntimeError("attn_mask's dimension {} is not supported".format(attn_mask.dim()))
@@ -267,17 +263,7 @@ def agent_aware_attention(query: Tensor,
         key_padding_mask = key_padding_mask.to(torch.bool)
 
     if bias_k is not None and bias_v is not None:
-        # if static_k is None and static_v is None:
-        #     k = torch.cat([k, bias_k.repeat(1, batch_size, 1)])
-        #     v = torch.cat([v, bias_v.repeat(1, batch_size, 1)])
-        #     if attn_mask is not None:
-        #         attn_mask = pad(attn_mask, (0, 1))
-        #     if key_padding_mask is not None:
-        #         key_padding_mask = pad(key_padding_mask, (0, 1))
-        # else:
-        #     assert static_k is None, "bias cannot be added to static key."
-        #     assert static_v is None, "bias cannot be added to static value."
-        raise NotImplementedError("NEED TO CHECK PROPER NAN PATTERN HANDLING IN THE FOLLOWING LINES OF CODE")
+        raise NotImplementedError
     else:
         assert bias_k is None
         assert bias_v is None
@@ -296,45 +282,25 @@ def agent_aware_attention(query: Tensor,
 
     # print(f"{static_k=}")
     if static_k is not None:
-        # assert static_k.size(0) == batch_size * num_heads
-        # assert static_k.size(2) == head_dim
-        # k = static_k
-        raise NotImplementedError("k becomes static_k, perhaps something needs to be done here wrt NaN patterns")
+        raise NotImplementedError
 
     # print(f"{static_v=}")
     if static_v is not None:
-        # assert static_v.size(0) == batch_size * num_heads
-        # assert static_v.size(2) == head_dim
-        # v = static_v
-        raise NotImplementedError("v becomes static_v, perhaps something needs to be done here wrt NaN patterns")
+        raise NotImplementedError
 
     src_len = k.size(1)
 
     # print(f"{key_padding_mask=}")
     if key_padding_mask is not None:
-        # assert key_padding_mask.size(0) == batch_size
-        # assert key_padding_mask.size(1) == src_len
         raise NotImplementedError
 
     # print(f"{add_zero_attn=}")
     if add_zero_attn:
-        # src_len += 1
-        # k = torch.cat([k, torch.zeros((k.size(0), 1) + k.size()[2:], dtype=k.dtype, device=k.device)], dim=1)
-        # v = torch.cat([v, torch.zeros((v.size(0), 1) + v.size()[2:], dtype=v.dtype, device=v.device)], dim=1)
-        # if attn_mask is not None:
-        #     attn_mask = pad(attn_mask, (0, 1))
-        # if key_padding_mask is not None:
-        #     key_padding_mask = pad(key_padding_mask, (0, 1))
-        raise NotImplementedError("NaN patterns, please check")
+        raise NotImplementedError
 
     # print(f"{gaussian_kernel=}")
     if gaussian_kernel:
-        # qk = torch.bmm(q, k.transpose(1, 2))
-        # q_n = q.pow(2).sum(dim=-1).unsqueeze(-1)
-        # k_n = k.pow(2).sum(dim=-1).unsqueeze(1)
-        # qk_dist = q_n + k_n - 2 * qk
-        # attn_output_weights = qk_dist * scaling * 0.5
-        raise NotImplementedError("check NaN patterns")
+        raise NotImplementedError
     else:
         attn_output_weights = torch.bmm(q, k.transpose(1, 2))       # [batch_size * num_heads, tgt_len, src_len]
 
@@ -367,23 +333,7 @@ def agent_aware_attention(query: Tensor,
         attn_output_weights = softmax(attn_output_weights, dim=-1)      # [batch_size * num_heads, tgt_len, src_len]
 
     else:
-        # if attn_mask is not None:
-        #     if attn_mask.dtype == torch.bool:
-        #         attn_output_weights.masked_fill_(attn_mask, float('-inf'))
-        #     else:
-        #         attn_output_weights += attn_mask
-        #
-        # if key_padding_mask is not None:
-        #     attn_output_weights = attn_output_weights.view(batch_size, num_heads, tgt_len, src_len)
-        #     attn_output_weights = attn_output_weights.masked_fill(
-        #         key_padding_mask.unsqueeze(1).unsqueeze(2),
-        #         float('-inf'),
-        #     )
-        #     attn_output_weights = attn_output_weights.view(batch_size * num_heads, tgt_len, src_len)
-        #
-        # attn_output_weights = softmax(
-        #     attn_output_weights, dim=-1)
-        raise NotImplementedError("Check Nan Pattern")
+        raise NotImplementedError
 
     attn_output_weights = dropout(attn_output_weights, p=dropout_p, training=training)
 
