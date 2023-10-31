e76d64fddf04845aa4586d47728546dd8d94f995
Paul Féry <paulfery4@gmail.com>
2023-10-31 12:23:46 +0100

    create simple torch version of geometric map class

--- data/map.py
+++ data/map.py
@@ -4,6 +4,8 @@ Code borrowed from Trajectron++: https://github.com/StanfordASL/Trajectron-plus-
 import torch
 import numpy as np
 from numpy.typing import NDArray
+Tensor = torch.Tensor
+import torchvision.transforms.functional
 import cv2
 import os
 from data.homography_warper import get_rotation_matrix2d, warp_affine_crop
@@ -295,3 +297,63 @@ class GeometricMap(Map):
         os.makedirs(os.path.dirname(fname), exist_ok=True)
         cv2.imwrite(fname, img)
 
+
+class TorchGeometricMap:
+
+    def __init__(self, map_image: Tensor, homography: Tensor = torch.eye(3)):
+        self.image = map_image          # [C, H, W]
+        self.homography = homography    # [3, 3]
+
+    def get_map_dimensions(self):
+        return self.image.shape[1:]     # [H, W]
+
+    def homography_translation(self, point: Tensor) -> None:
+        # point [2]
+        self.homography[:2, 2] += point
+
+    def homography_scaling(self, factor: float) -> None:
+        self.homography[0, 0] *= factor
+        self.homography[1, 1] *= factor
+
+    def rotate_around_center(self, theta: float) -> None:
+        self.image = torchvision.transforms.functional.rotate(self.image, angle=theta)
+        cy, cx = self.get_map_dimensions()
+
+        cx *= 0.5
+        cy *= 0.5
+
+        # angle is in degrees, needs to be converted to rad first, we apply the negative of the angle to remain
+        # consistent with the image representation of the scene.
+        # due to the fact the coordinate system of the image is flipped (origin in the top left corner).
+        cos = np.cos(-theta * np.pi * 0.0055555555555555555555555555555556)
+        sin = np.sin(-theta * np.pi * 0.0055555555555555555555555555555556)
+
+        matrix = torch.Tensor(
+            [[cos, -sin, -cos * cx + sin * cy + cx],
+             [sin, cos, -sin * cx - cos * cy + cy],
+             [0., 0., 1.]]
+        )
+        self.homography = matrix @ self.homography
+
+    def crop(self, crop_coords: Tensor, resolution: int):
+        """
+        Careful: This method is not responsible for changing the homography matrix
+        after the cropping has been performed.
+        To maintain consistency, you need to compute the corresponding homography and use set_homography.
+        """
+        top = torch.round(crop_coords[0, 1]).to(torch.int64)
+        left = torch.round(crop_coords[0, 0]).to(torch.int64)
+        side = torch.round(crop_coords[1, 0] - crop_coords[0, 0]).to(torch.int64)
+
+        self.image = torchvision.transforms.functional.resized_crop(
+            self.image, top=top, left=left, height=side, width=side, size=resolution
+        )
+
+    def to_map_points(self, points: Tensor) -> Tensor:
+        # points [*, 2]
+        homogeneous_points = torch.cat((points, torch.ones([*points.shape[:-1], 1])), dim=-1).transpose(-1, -2)
+        mapped_points = (self.homography @ homogeneous_points).transpose(-1, -2)[..., :-1]
+        return mapped_points
+
+    def set_homography(self, matrix: Tensor) -> None:
+        self.homography = matrix
