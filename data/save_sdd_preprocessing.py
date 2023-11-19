3880b403fd466a89be232a52edca09417f0a6212
Paul Féry <paulfery4@gmail.com>
2023-11-19 21:20:35 +0100

    upgrade script to accept dataset specifications as arguments.

--- data/save_sdd_preprocessing.py
+++ data/save_sdd_preprocessing.py
@@ -12,6 +12,7 @@ Total dataset sizes:
 """
 
 if __name__ == '__main__':
+    import argparse
     import os.path
     import pickle
     import numpy as np
@@ -20,78 +21,69 @@ if __name__ == '__main__':
     from utils.config import Config, REPO_ROOT
     from utils.utils import prepare_seed
 
-    config_str = 'sdd_baseline_occlusionformer_pre'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--occlusion_process', default=None)
+    parser.add_argument('--split', default=None)
+    parser.add_argument('--start_idx', default=None)
+    parser.add_argument('--end_idx', default=None)
+    args = parser.parse_args()
 
+    assert args.occlusion_process in ['occlusion_simulation', 'fully_observed']
+    assert args.split in ['train', 'val', 'test']
+    if isinstance(args.start_idx, int) and isinstance(args.end_idx, int):
+        assert args.end_idx > args.start_idx
+
+    dataset_id = args.occlusion_process
+    split = args.split
+    start_idx = args.start_idx
+    end_idx = args.end_idx
+
+    config_str = 'sdd_baseline_occlusionformer_pre'
     config = Config(config_str)
 
-    splits = ['train', 'val', 'test']
     presaved_datasets_dir = os.path.join(REPO_ROOT, 'datasets', 'SDD', 'pre_saved_datasets')
 
-    # for split in splits:
-    #     dataset_id = 'occlusion_simulation'
-    #     print(f"Presaving the \'{dataset_id}\' dataset.")
-    #     print(f"Beginning saving process of {split} split.")
-    #
-    #     prepare_seed(config.seed)
-    #     generator = TorchDataGeneratorSDD(parser=config, log=None, split=split)
-    #
-    #     save_path = os.path.join(presaved_datasets_dir, dataset_id, split)
-    #     print(f"root directory of the dataset will be:\n{save_path}")
-    #     os.makedirs(save_path, exist_ok=True)
-    #
-    #     # indices = np.linspace(0, len(generator), 50, endpoint=False).astype(int)
-    #     indices = range(len(generator))
-    #
-    #     for idx in tqdm(indices):
-    #
-    #         filename = f'{idx:08}.pickle'
-    #         data_dict = generator.__getitem__(idx)
-    #
-    #         del data_dict['timesteps']
-    #         del data_dict['scene_orig']
-    #
-    #         with open(os.path.join(save_path, filename), 'wb') as f:
-    #             pickle.dump(data_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
-    #
-    #     print(f"Done!")
+    del_keys = ['timesteps',
+                'scene_orig']
+    if args.occlusion_process == 'fully_observed':
+        del_keys.extend(
+            ['occlusion_map',
+             'dist_transformed_occlusion_map',
+             'probability_occlusion_map',
+             'nlog_probability_occlusion_map',
+             'scene_map',
+             'map_homography']
+        )
 
-    config = Config(config_str)
+    print(f"Presaving the \'{dataset_id}\' dataset.")
+    print(f"Beginning saving process of {split} split.")
 
-    for split in splits:
-        dataset_id = 'fully_observed'
-        config.occlusion_process = 'fully_observed'
-        print(f"Presaving the \'{dataset_id}\' dataset.")
-        print(f"Beginning saving process of {split} split.")
+    prepare_seed(config.seed)
 
-        prepare_seed(config.seed)
-        generator = TorchDataGeneratorSDD(parser=config, log=None, split=split)
+    generator = TorchDataGeneratorSDD(parser=config, log=None, split=split)
 
-        save_path = os.path.join(presaved_datasets_dir, dataset_id, split)
-        print(f"root directory of the dataset will be:\n{save_path}")
-        os.makedirs(save_path, exist_ok=True)
+    save_path = os.path.join(presaved_datasets_dir, dataset_id, split)
+    print(f"root directory of the dataset will be:\n{save_path}")
+    os.makedirs(save_path, exist_ok=True)
 
-        # indices = np.linspace(0, len(generator), 50, endpoint=False).astype(int)
-        indices = range(len(generator))
+    if start_idx is None:
+        start_idx = 0
 
-        for idx in tqdm(indices):
+    if end_idx is None:
+        end_idx = len(generator)
 
-            filename = f'{idx:08}.pickle'
-            data_dict = generator.__getitem__(idx)
+    indices = range(start_idx, end_idx, 1)
 
-            del data_dict['timesteps']
-            del data_dict['scene_orig']
+    for idx in tqdm(indices):
 
-            del data_dict['occlusion_map']
-            del data_dict['dist_transformed_occlusion_map']
-            del data_dict['probability_occlusion_map']
-            del data_dict['nlog_probability_occlusion_map']
-            del data_dict['scene_map']
-            del data_dict['map_homography']
+        filename = f'{idx:08}.pickle'
+        data_dict = generator.__getitem__(idx)
 
-            with open(os.path.join(save_path, filename), 'wb') as f:
-                pickle.dump(data_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
+        for key in del_keys:
+            del data_dict[key]
 
-        print(f"Done!")
+        with open(os.path.join(save_path, filename), 'wb') as f:
+            pickle.dump(data_dict, f, protocol=pickle.HIGHEST_PROTOCOL)
 
+    print(f"Done!")
     print(f"Goodbye !")
-
