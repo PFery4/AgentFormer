27fb76ac440671c87b8d9e46ddb974272bbceef4
Paul Féry <paulfery4@gmail.com>
2023-11-20 12:12:26 +0100

    create simple script to visualize dataset

---
+++ visualize_dataset.py
@@ -0,0 +1,8 @@
+from utils.sdd_visualize import show_example_instances_dataloader
+
+if __name__ == '__main__':
+    print("Hello!")
+
+    show_example_instances_dataloader()
+
+    print("Goodbye!")
