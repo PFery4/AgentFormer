b17143360cf682d26858c1eb040c03550444ff85
Paul Féry <paulfery4@gmail.com>
2023-11-20 12:10:33 +0100

    remove visualization functions (transferred to sdd_visualize.py file, implement dataset class for extracting presaved data.

--- data/sdd_dataloader.py
+++ data/sdd_dataloader.py
@@ -532,153 +532,6 @@ class TorchDataGeneratorSDD(Dataset):
 
         return data_dict
 
-    @staticmethod
-    def visualize_sequences(data_dict: Dict, draw_ax: matplotlib.axes.Axes):
-        ids = data_dict['identities']
-
-        obs_ids = data_dict['obs_identity_sequence']
-        obs_trajs = data_dict['obs_position_sequence']
-        obs_vel = data_dict['obs_velocity_sequence']
-        obs_timesteps = data_dict['obs_timestep_sequence']
-        last_obs_pos = data_dict['last_obs_positions']
-
-        pred_ids = data_dict['pred_identity_sequence']
-        pred_trajs = data_dict['pred_position_sequence']
-        pred_vel = data_dict['pred_velocity_sequence']
-        pred_timesteps = data_dict['pred_timestep_sequence']
-
-        homography = data_dict['map_homography']
-
-        vel_homography = homography.clone()
-        vel_homography[:2, 2] = 0.
-
-        color = plt.cm.rainbow(np.linspace(0, 1, ids.shape[0]))
-
-        homogeneous_last_obs_pos = torch.cat((last_obs_pos, torch.ones([*last_obs_pos.shape[:-1], 1])),
-                                             dim=-1).transpose(-1, -2)
-        plot_last_obs_pos = (homography @ homogeneous_last_obs_pos).transpose(-1, -2)[..., :-1]
-
-        for i, (ag_id, last_pos) in enumerate(zip(ids, plot_last_obs_pos)):
-            c = color[i].reshape(1, -1)
-            draw_ax.scatter(last_pos[0], last_pos[1], s=70, facecolors='none', edgecolors=c, alpha=0.3)
-
-        homogeneous_obs_trajs = torch.cat((obs_trajs, torch.ones([*obs_trajs.shape[:-1], 1])), dim=-1).transpose(-1, -2)
-        plot_obs_trajs = (homography @ homogeneous_obs_trajs).transpose(-1, -2)[..., :-1]
-
-        homogeneous_obs_vel = torch.cat((obs_vel, torch.ones([*obs_vel.shape[:-1], 1])), dim=-1).transpose(-1, -2)
-        plot_obs_vel = (vel_homography @ homogeneous_obs_vel).transpose(-1, -2)[..., :-1]
-
-        for i, (ag_id, pos, vel, timestep) in enumerate(zip(obs_ids, plot_obs_trajs, plot_obs_vel, obs_timesteps)):
-            c = color[np.nonzero(ids == ag_id).flatten()].reshape(1, -1)
-            s = 5 * (timestep + 8)
-            draw_ax.scatter(pos[0], pos[1], marker='x', s=5 + s, color=c)
-            old_pos = pos - vel
-            draw_ax.plot([old_pos[0], pos[0]], [old_pos[1], pos[1]], color=c, linestyle='--', alpha=0.8)
-
-        homogeneous_pred_trajs = torch.cat((pred_trajs, torch.ones([*pred_trajs.shape[:-1], 1])), dim=-1).transpose(-1,
-                                                                                                                    -2)
-        plot_pred_trajs = (homography @ homogeneous_pred_trajs).transpose(-1, -2)[..., :-1]
-
-        homogeneous_pred_vel = torch.cat((pred_vel, torch.ones([*pred_vel.shape[:-1], 1])), dim=-1).transpose(-1, -2)
-        plot_pred_vel = (vel_homography @ homogeneous_pred_vel).transpose(-1, -2)[..., :-1]
-
-        for i, (ag_id, pos, vel, timestep) in enumerate(zip(pred_ids, plot_pred_trajs, plot_pred_vel, pred_timesteps)):
-            c = color[np.nonzero(ids == ag_id).flatten()].reshape(1, -1)
-            s = 5 * (timestep)
-            draw_ax.scatter(pos[0], pos[1], marker='*', s=5 + s, color=c)
-            old_pos = pos - vel
-            draw_ax.plot([old_pos[0], pos[0]], [old_pos[1], pos[1]], color=c, linestyle=':', alpha=0.8)
-
-    @staticmethod
-    def visualize_trajectories(data_dict: Dict, draw_ax: matplotlib.axes.Axes) -> None:
-        ids = data_dict['identities']
-        trajs = data_dict['trajectories']
-        obs_mask = data_dict['observation_mask']
-        homography = data_dict['map_homography']
-
-        homogeneous_trajs = torch.cat((trajs, torch.ones([*trajs.shape[:-1], 1])), dim=-1).transpose(-1, -2)
-        plot_trajs = (homography @ homogeneous_trajs).transpose(-1, -2)[..., :-1]
-
-        color_iter = iter(plt.cm.rainbow(np.linspace(0, 1, ids.shape[0])))
-        for traj, mask in zip(plot_trajs, obs_mask):
-            c = next(color_iter).reshape(1, -1)
-            draw_ax.scatter(traj[:, 0][mask], traj[:, 1][mask], marker='x', s=20, color=c)
-            draw_ax.scatter(traj[:, 0][~mask], traj[:, 1][~mask], marker='*', s=20, color=c)
-
-    @staticmethod
-    def visualize_map(data_dict: Dict, draw_ax: matplotlib.axes.Axes) -> None:
-        scene_map_img = data_dict['scene_map']  # [C, H, W]
-        occlusion_map_img = data_dict['occlusion_map']  # [H, W]
-
-        draw_ax.set_xlim(0., scene_map_img.shape[2])
-        draw_ax.set_ylim(scene_map_img.shape[1], 0.)
-        draw_ax.imshow(scene_map_img.permute(1, 2, 0))
-
-        occlusion_map_render = np.full(
-            (*occlusion_map_img.shape, 4), (1., 0, 0, 0.3)
-        ) * (~occlusion_map_img)[..., None].numpy()
-        draw_ax.imshow(occlusion_map_render)
-
-    @staticmethod
-    def visualize_dist_transformed_occlusion_map(data_dict: Dict, draw_ax: matplotlib.axes.Axes) -> None:
-        dt_occlusion_map = data_dict['dist_transformed_occlusion_map']
-
-        divider = mpl_toolkits.axes_grid1.make_axes_locatable(draw_ax)
-        colors_visible = plt.cm.Purples(np.linspace(0.5, 1, 256))
-        colors_occluded = plt.cm.Reds(np.linspace(1, 0.5, 256))
-        all_colors = np.vstack((colors_occluded, colors_visible))
-        color_map = colors.LinearSegmentedColormap.from_list('color_map', all_colors)
-        divnorm = colors.TwoSlopeNorm(
-            vmin=np.min([-1, torch.min(dt_occlusion_map)]),
-            vcenter=0.0,
-            vmax=np.max([1, torch.max(dt_occlusion_map)])
-        )
-        cax = divider.append_axes('right', size='5%', pad=0.05)
-        img = draw_ax.imshow(dt_occlusion_map, norm=divnorm, cmap=color_map)
-        draw_ax.get_figure().colorbar(img, cax=cax, orientation='vertical')
-
-    @staticmethod
-    def visualize_probability_map(data_dict: Dict, draw_ax: matplotlib.axes.Axes) -> None:
-        probability_map = data_dict['probability_occlusion_map']
-
-        divider = mpl_toolkits.axes_grid1.make_axes_locatable(draw_ax)
-        cax = divider.append_axes('right', size='5%', pad=0.05)
-        img = draw_ax.imshow(probability_map, cmap='Greys')
-        draw_ax.get_figure().colorbar(img, cax=cax, orientation='vertical')
-
-    @staticmethod
-    def visualize_nlog_probability_map(data_dict: Dict, draw_ax: matplotlib.axes.Axes) -> None:
-        nlog_probability_map = data_dict['nlog_probability_occlusion_map']
-
-        divider = mpl_toolkits.axes_grid1.make_axes_locatable(draw_ax)
-        cax = divider.append_axes('right', size='5%', pad=0.05)
-        img = draw_ax.imshow(nlog_probability_map, cmap='Greys')
-        draw_ax.get_figure().colorbar(img, cax=cax, orientation='vertical')
-
-    def visualize(
-            self,
-            data_dict: Dict,
-            draw_ax: matplotlib.axes.Axes,
-            draw_ax_sequences: Optional[matplotlib.axes.Axes] = None,
-            draw_ax_dist_transformed_map: Optional[matplotlib.axes.Axes] = None,
-            draw_ax_probability_map: Optional[matplotlib.axes.Axes] = None,
-            draw_ax_nlog_probability_map: Optional[matplotlib.axes.Axes] = None
-    ) -> None:
-        self.visualize_map(data_dict=data_dict, draw_ax=draw_ax)
-        self.visualize_trajectories(data_dict=data_dict, draw_ax=draw_ax)
-        if draw_ax_sequences is not None:
-            self.visualize_map(data_dict=data_dict, draw_ax=draw_ax_sequences)
-            self.visualize_sequences(data_dict=data_dict, draw_ax=draw_ax_sequences)
-
-        if draw_ax_dist_transformed_map is not None:
-            self.visualize_dist_transformed_occlusion_map(data_dict=data_dict, draw_ax=draw_ax_dist_transformed_map)
-
-        if draw_ax_probability_map is not None:
-            self.visualize_probability_map(data_dict=data_dict, draw_ax=draw_ax_probability_map)
-
-        if draw_ax_nlog_probability_map is not None:
-            self.visualize_nlog_probability_map(data_dict=data_dict, draw_ax=draw_ax_nlog_probability_map)
-
 
 class PresavedDatasetSDD(Dataset):
     # TODO: add map homography
@@ -695,10 +548,20 @@ class PresavedDatasetSDD(Dataset):
         prnt_str = "\n-------------------------- loading %s data --------------------------" % split
         print_log(prnt_str, log=log) if log is not None else print(prnt_str)
 
-        self.dataset_dir = os.path.join(self.presaved_datasets_dir, parser.occlusion_process, split)
+        self.occlusion_process = parser.get('occlusion_process', 'fully_observed')
+        self.dataset_dir = os.path.join(self.presaved_datasets_dir, self.occlusion_process, split)
 
         prnt_str = f"Extracting data from the following directory:\n{self.dataset_dir}"
         print_log(prnt_str, log=log) if log is not None else print(prnt_str)
+        self.pickle_files = glob.glob1(self.dataset_dir, "*.pickle")
+
+        self.map_side = parser.get('scene_side_length', 80.0)               # [m]
+        self.map_resolution = parser.get('global_map_resolution', 800)      # [px]
+        self.map_homography = torch.Tensor(
+            [[self.map_resolution / self.map_side, 0., self.map_resolution / 2],
+             [0., self.map_resolution / self.map_side, self.map_resolution / 2],
+             [0., 0., 1.]]
+        )
 
         self.T_obs = parser.past_frames
         self.T_pred = parser.future_frames
@@ -712,13 +575,13 @@ class PresavedDatasetSDD(Dataset):
         print_log(prnt_str, log=log) if log is not None else print(prnt_str)
 
     def __len__(self):
-        return len(glob.glob1(self.dataset_dir, "*.pickle"))
+        return len(self.pickle_files)
 
     def __getitem__(self, idx):
 
-        filename = f'{idx:08}.pickle'
+        filename = self.pickle_files[idx]
 
-        with open(os.path.join(self.dataset_dir, filename), 'wb') as f:
+        with open(os.path.join(self.dataset_dir, filename), 'rb') as f:
             data_dict = pickle.load(f)
 
         data_dict['timesteps'] = self.timesteps
@@ -781,5 +644,10 @@ def show_example_instances_dataloader():
 
 
 if __name__ == '__main__':
-    show_example_instances_dataloader()
+    # show_example_instances_dataloader()
     # save_preprocessed_dataset()
+
+    config_str = 'sdd_baseline_occlusionformer_pre'
+    config = Config(config_str)
+    PresavedDatasetSDD(parser=config, log=None, split='train')
+
