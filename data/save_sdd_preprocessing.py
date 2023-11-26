92925f9b45cfd8e4134b4c660943872eb05409ab
Paul Féry <paulfery4@gmail.com>
2023-11-26 11:45:02 +0100

    fix saving file: now directly using the config file. This prevents the saving of the dataset to incorrecly save occlusiondata when occlusion_process=True

--- data/save_sdd_preprocessing.py
+++ data/save_sdd_preprocessing.py
@@ -22,32 +22,33 @@ if __name__ == '__main__':
     from utils.utils import prepare_seed
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--occlusion_process', default=None)
-    parser.add_argument('--split', default=None)
+    parser.add_argument('--cfg', default=None)
+    parser.add_argument('--split', default='train')
     parser.add_argument('--start_idx', type=int, default=0)
     parser.add_argument('--end_idx', type=int, default=-10)
     parser.add_argument('--tiny_dataset', type=bool, default=False)
     args = parser.parse_args()
 
-    assert args.occlusion_process in ['occlusion_simulation', 'fully_observed']
     assert args.split in ['train', 'val', 'test']
     if args.end_idx > 0:
         assert args.end_idx > args.start_idx
 
-    dataset_id = args.occlusion_process
+    # dataset_id = args.occlusion_process
+    cfg = args.cfg
     split = args.split
-    start_idx = args.start_idx
-    end_idx = args.end_idx
-    tiny = args.tiny_dataset
+    if args.tiny_dataset:
+        tiny, start_idx, end_idx = True, None, None
+    else:
+        tiny, start_idx, end_idx = False, args.start_idx, args.end_idx
 
-    config_str = 'sdd_baseline_occlusionformer_pre'
-    config = Config(config_str)
+    config = Config(cfg_id=cfg)
 
     presaved_datasets_dir = os.path.join(REPO_ROOT, 'datasets', 'SDD', 'pre_saved_datasets')
 
     del_keys = ['timesteps',
                 'scene_orig']
-    if args.occlusion_process == 'fully_observed':
+    if config.occlusion_process == 'fully_observed':
+        print(f"Fully observed dataset: no map data will be saved")
         del_keys.extend(
             ['occlusion_map',
              'dist_transformed_occlusion_map',
@@ -57,25 +58,28 @@ if __name__ == '__main__':
              'map_homography']
         )
 
-    print(f"Presaving the \'{dataset_id}\' dataset.")
+    print(f"Presaving a dataset from the \'{cfg}\' file.")
     print(f"Beginning saving process of {split} split.")
 
     prepare_seed(config.seed)
 
     generator = TorchDataGeneratorSDD(parser=config, log=None, split=split)
 
-    save_path = os.path.join(presaved_datasets_dir, dataset_id, split)
+    save_dir_name = f"{config.occlusion_process}_tiny" if tiny else config.occlusion_process
+
+    save_path = os.path.join(presaved_datasets_dir, save_dir_name, split)
     print(f"root directory of the dataset will be:\n{save_path}")
     os.makedirs(save_path, exist_ok=True)
 
-    if end_idx < 0:
-        end_idx = len(generator)
-
-    indices = range(start_idx, end_idx, 1)
-
+    indices = None
     if tiny:
         indices = np.linspace(0, len(generator), num=50, endpoint=False).astype(int)
-        print("Attention! Creating a tiny version of the dataset: only 50 instances will be saved.")
+        print("Creating a tiny version of the dataset: only 50 instances will be saved.")
+    else:
+        if end_idx < 0:
+            end_idx = len(generator)
+        indices = range(start_idx, end_idx, 1)
+        print(f"Saving Dataset instances between the range [{start_idx}-{end_idx}].")
 
     for idx in tqdm(indices):
 
