07ff647be8e1ac269399caee8ceab3175de5b0ae
Paul Féry <paulfery4@gmail.com>
2023-12-01 17:05:44 +0100

    add untrained models to the entries of the model_lib dictionary

--- model/model_lib.py
+++ model/model_lib.py
@@ -1,8 +1,11 @@
 from model.agentformer import AgentFormer
 from model.dlow import DLow
+from model.untrained_models import Oracle, ConstantVelocityPredictor
 
 
 model_dict = {
     'agentformer': AgentFormer,
-    'dlow': DLow
+    'dlow': DLow,
+    'oracle': Oracle,
+    'const_velocity': ConstantVelocityPredictor
 }
