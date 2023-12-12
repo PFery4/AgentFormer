532f71186e3b1336c34abef3bb2ca46b4d935bd6
Paul Féry <paulfery4@gmail.com>
2023-12-12 14:27:38 +0100

    add options to profile either the dataset class, or the model.

--- profiler_script.py
+++ profiler_script.py
@@ -16,8 +16,12 @@ from train import print_log
 
 if __name__ == '__main__':
 
-    model_runs = 10
-    cfg_str = 'sdd_baseline_copy_idgaf_pre'
+    model_runs = 50
+    cfg_str = 'sdd_baseline_copy_for_test_pre'
+    profile_model = False
+    profile_dataset = True
+    presaved_dataset = False
+
     cfg = Config(cfg_id=cfg_str, tmp=True, create_dirs=True)
 
     prepare_seed(cfg.seed)
@@ -55,8 +59,10 @@ if __name__ == '__main__':
     tb_logger = SummaryWriter(cfg.tb_dir)
     print(f"{cfg.tb_dir=}")
 
-    sdd_dataset = PresavedDatasetSDD(parser=cfg, log=log, split='train')
-    training_loader = DataLoader(dataset=sdd_dataset, shuffle=True, num_workers=2)
+    dataset_class = PresavedDatasetSDD if presaved_dataset else TorchDataGeneratorSDD
+    sdd_dataset = dataset_class(parser=cfg, log=log, split='train')
+    print(f"dataset class is of type: {type(sdd_dataset)}")
+    training_loader = DataLoader(dataset=sdd_dataset, shuffle=True, num_workers=1)
 
     model_id = cfg.get('model_id', 'agentformer')
     model = model_dict[model_id](cfg)
@@ -69,33 +75,47 @@ if __name__ == '__main__':
     else:
         raise ValueError('unknown scheduler type!')
 
-    model.set_device(device)
     print(f"{device=}")
+    model.set_device(device)
     model.train()
 
     ###################################################################################################################
-    print(f"NOW PROFILING MODEL ON ITS OWN")
     since_train = time.time()
-    train_loss_meter = {x: AverageMeter() for x in cfg.loss_cfg.keys()}
-    train_loss_meter['total_loss'] = AverageMeter()
+    if profile_model:
+        print(f"NOW PROFILING THE MODEL")
+        train_loss_meter = {x: AverageMeter() for x in cfg.loss_cfg.keys()}
+        train_loss_meter['total_loss'] = AverageMeter()
 
-    data_iter = iter(training_loader)
+        data_iter = iter(training_loader)
 
-    for i in range(model_runs):
+        for i in range(model_runs):
 
-        data = next(data_iter)
+            data = next(data_iter)
 
-        total_loss, loss_dict, loss_unweighted_dict = train.train_one_batch(
-            model=model, data=data, optimizer=optimizer
-        )
+            total_loss, loss_dict, loss_unweighted_dict = train.train_one_batch(
+                model=model, data=data, optimizer=optimizer
+            )
 
-        train.update_loss_meters(
-            loss_meter=train_loss_meter,
-            total_loss=total_loss,
-            loss_unweighted_dict=loss_unweighted_dict
-        )
+            train.update_loss_meters(
+                loss_meter=train_loss_meter,
+                total_loss=total_loss,
+                loss_unweighted_dict=loss_unweighted_dict
+            )
 
-        print(f"{i, total_loss=}")
+            # print(f"{i, total_loss=}")
 
-    print(f"Passing through {model_runs} instances in: {time.time() - since_train} seconds")
+        print(f"Passing through {model_runs} instances in: {time.time() - since_train} seconds")
+
+    if profile_dataset:
+        print(f"NOW PROFILING THE DATASET")
+        import numpy as np
+
+        indices = np.random.randint(len(sdd_dataset), size=model_runs)
+
+        for i, idx in enumerate(indices):
+            data = sdd_dataset.__getitem__(idx)
+            print(f"{i=}")
+
+        print(f"Passing through {model_runs} instances in: {time.time() - since_train} seconds")
+
     print("Goodbye!")
