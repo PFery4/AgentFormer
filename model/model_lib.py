0fbd4de45687e1d25a3632021f0df543fe4c46b3
Paul Féry <paulfery4@gmail.com>
2024-01-10 17:45:41 +0100

    update model_lib dictionary to contain the wrapper classes for the original AgentFormer and Dlow models

--- model/model_lib.py
+++ model/model_lib.py
@@ -1,11 +1,14 @@
 from model.agentformer import AgentFormer
 from model.dlow import DLow
+from model.original_agentformer import OrigAgentFormerWrapper, OrigDLowWrapper
 from model.untrained_models import Oracle, ConstantVelocityPredictor
 
 
 model_dict = {
     'agentformer': AgentFormer,
     'dlow': DLow,
+    'orig_agentformer': OrigAgentFormerWrapper,
+    'orig_dlow': OrigDLowWrapper,
     'oracle': Oracle,
     'const_velocity': ConstantVelocityPredictor
 }
