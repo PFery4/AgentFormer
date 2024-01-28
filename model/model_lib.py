52cb6410151c593db74099c0d267f1b3690f09e0
Paul Fery <paulfery4@gmail.com>
2024-01-28 18:14:52 +0100

    modify model_dict, to retrieve the proper DLow wrapper

--- model/model_lib.py
+++ model/model_lib.py
@@ -1,6 +1,7 @@
 from model.agentformer import AgentFormer
 from model.dlow import DLow
 from model.original_agentformer import OrigModelWrapper
+from model.original_dlow import OrigDLowWrapper
 from model.untrained_models import Oracle, ConstantVelocityPredictor
 
 
@@ -8,7 +9,7 @@ model_dict = {
     'agentformer': AgentFormer,
     'dlow': DLow,
     'orig_agentformer': OrigModelWrapper,
-    'orig_dlow': OrigModelWrapper,
+    'orig_dlow': OrigDLowWrapper,
     'oracle': Oracle,
     'const_velocity': ConstantVelocityPredictor
 }
