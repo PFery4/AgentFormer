844af15f3986fd4362af767dc2fca3f6afb637bc
Paul Fery <paulfery4@gmail.com>
2024-02-26 23:02:29 +0100

    add option to pass multiple configs

--- plot_loss_graph.py
+++ plot_loss_graph.py
@@ -9,108 +9,110 @@ from utils.config import Config, REPO_ROOT
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', default=None)
+    parser.add_argument('--cfg', nargs='+', default=None)
     parser.add_argument('--split', nargs='+', default=None)
     parser.add_argument('--save', action='store_true', default=False)
     parser.add_argument('--show', action='store_true', default=False)
     parser.add_argument('--weighted_losses', type=bool, default=True)
     args = parser.parse_args()
 
+    print(args.cfg)
     print(args.split)
     assert all([name in ['train', 'val'] for name in args.split])
     assert args.save or args.show, "You must choose to either *show* or *save* the loss graph..."
-    cfg = Config(args.cfg)
-
     color_maps = {'train': 'cool', 'val': 'autumn'}
 
-    # create figure
-    fig, ax = plt.subplots()
+    for cfg_str in args.cfg:
+        cfg = Config(cfg_str)
 
-    for split in args.split:
-        csv_file = os.path.join(cfg.log_dir, f'{split}_losses.csv')
+        # create figure
+        fig, ax = plt.subplots()
 
-        assert os.path.exists(csv_file)
+        for split in args.split:
+            csv_file = os.path.join(cfg.log_dir, f'{split}_losses.csv')
 
-        df = pd.read_csv(csv_file)
+            assert os.path.exists(csv_file)
 
-        # # wip to show example of corrupted dataframe
-        # lines = df.iloc[[12, 13, 14, 15]]
-        # lines.index = [15.1, 15.2, 15.3, 15.4]
-        # df = pd.concat([df, lines])
-        # df.sort_index(inplace=True)
-        # df.reset_index(inplace=True, drop=True)
-        # lines = df.iloc[[29, 30, 31]]
-        # lines.index = [31.1, 31.2, 31.3]
-        # df = pd.concat([df, lines])
-        # df.sort_index(inplace=True)
-        # df.reset_index(inplace=True, drop=True)
-        # df.loc[10000] = ['tb_x', 'epoch', 'batch', 'total_loss', 'mse', 'kld', 'sample']
-        # df = df.sort_index().reset_index(drop=True)
-        # print(df)
+            df = pd.read_csv(csv_file)
 
-        # Beginning preprocessing
-        # Remove rows with same content as columns
-        df = df[~(df == df.columns).all(axis=1)]
+            # # wip to show example of corrupted dataframe
+            # lines = df.iloc[[12, 13, 14, 15]]
+            # lines.index = [15.1, 15.2, 15.3, 15.4]
+            # df = pd.concat([df, lines])
+            # df.sort_index(inplace=True)
+            # df.reset_index(inplace=True, drop=True)
+            # lines = df.iloc[[29, 30, 31]]
+            # lines.index = [31.1, 31.2, 31.3]
+            # df = pd.concat([df, lines])
+            # df.sort_index(inplace=True)
+            # df.reset_index(inplace=True, drop=True)
+            # df.loc[10000] = ['tb_x', 'epoch', 'batch', 'total_loss', 'mse', 'kld', 'sample']
+            # df = df.sort_index().reset_index(drop=True)
+            # print(df)
 
-        # Remove end of epoch placeholder rows
-        df = df[~(df['tb_x'].str.contains('END OF EPOCH'))]
-        df.reset_index(inplace=True, drop=True)
+            # Beginning preprocessing
+            # Remove rows with same content as columns
+            df = df[~(df == df.columns).all(axis=1)]
 
-        # for debugging, please remove once you run on actual proper csvs
-        if 'epoch' not in df.columns:
-            df = df.astype({'tb_x': int})
-            df['epoch'] = (df['tb_x'] - 1) // 85776
-            df['batch'] = (df['tb_x'] - 1) % 85776
-        # end of debug snippet
+            # Remove end of epoch placeholder rows
+            df = df[~(df['tb_x'].str.contains('END OF EPOCH'))]
+            df.reset_index(inplace=True, drop=True)
 
-        # extract the loss names
-        loss_names = [name for name in df.columns.tolist() if name not in ['tb_x', 'epoch', 'batch']]
-        df = df.astype({'tb_x': int, 'epoch': int, 'batch': int})
-        df = df.astype({name: float for name in loss_names})
+            # for debugging, please remove once you run on actual proper csvs
+            if 'epoch' not in df.columns:
+                df = df.astype({'tb_x': int})
+                df['epoch'] = (df['tb_x'] - 1) // 85776
+                df['batch'] = (df['tb_x'] - 1) % 85776
+            # end of debug snippet
 
-        # multiply unweighted loss values by their weight factor
-        loss_weights = {name: loss_dict['weight'] for name, loss_dict in cfg.loss_cfg.items()}
-        if args.weighted_losses:
-            for name, weight in loss_weights.items():
-                df[name] *= weight
+            # extract the loss names
+            loss_names = [name for name in df.columns.tolist() if name not in ['tb_x', 'epoch', 'batch']]
+            df = df.astype({'tb_x': int, 'epoch': int, 'batch': int})
+            df = df.astype({name: float for name in loss_names})
 
-        # get the x-axis values corresponding to the beginning of a new epoch
-        new_epochs_tb_x = df['tb_x'][~(df['epoch'].eq(df['epoch'].shift()))].to_numpy()
+            # multiply unweighted loss values by their weight factor
+            loss_weights = {name: loss_dict['weight'] for name, loss_dict in cfg.loss_cfg.items()}
+            if args.weighted_losses:
+                for name, weight in loss_weights.items():
+                    df[name] *= weight
 
-        # identify the sequences that have been overwritten due to a rerun of the model from a previous checkpoint
-        overwritten_rows = (df.groupby('tb_x').cumcount(ascending=False) != 0).to_numpy()
-        kept_rows = (~overwritten_rows).nonzero()[0]
-        seq_indices = (overwritten_rows[1:] != overwritten_rows[:-1])[overwritten_rows[1:]]
-        seq_indices = seq_indices.nonzero()[0]
-        seq_indices = np.append(seq_indices, overwritten_rows.nonzero()[0].shape[0])
-        overwritten_rows = overwritten_rows.nonzero()[0]
-        overwritten_sequences = [overwritten_rows[start:end] for start, end in zip(seq_indices[:-1], seq_indices[1:])]
+            # get the x-axis values corresponding to the beginning of a new epoch
+            new_epochs_tb_x = df['tb_x'][~(df['epoch'].eq(df['epoch'].shift()))].to_numpy()
 
-        colors = iter(mpl.colormaps[color_maps[split]](np.linspace(0, 1, len(loss_names))))
+            # identify the sequences that have been overwritten due to a rerun of the model from a previous checkpoint
+            overwritten_rows = (df.groupby('tb_x').cumcount(ascending=False) != 0).to_numpy()
+            kept_rows = (~overwritten_rows).nonzero()[0]
+            seq_indices = (overwritten_rows[1:] != overwritten_rows[:-1])[overwritten_rows[1:]]
+            seq_indices = seq_indices.nonzero()[0]
+            seq_indices = np.append(seq_indices, overwritten_rows.nonzero()[0].shape[0])
+            overwritten_rows = overwritten_rows.nonzero()[0]
+            overwritten_sequences = [overwritten_rows[start:end] for start, end in zip(seq_indices[:-1], seq_indices[1:])]
 
-        for loss in loss_names:
-            c = next(colors)
-            values = df[loss].to_numpy()
+            colors = iter(mpl.colormaps[color_maps[split]](np.linspace(0, 1, len(loss_names))))
 
-            for sequence in overwritten_sequences:
-                ax.plot(df['tb_x'].to_numpy()[sequence], values[sequence], c=c, alpha=0.4)
-            ax.plot(df['tb_x'].to_numpy()[kept_rows], values[kept_rows], c=c, label=f"{split}_{loss}")
+            for loss in loss_names:
+                c = next(colors)
+                values = df[loss].to_numpy()
 
-        ax.set_xticks(new_epochs_tb_x, minor=True)
-        ax.xaxis.grid(True, which='minor')
-        fig.set_size_inches(16, 9)
-        plt.legend(loc='upper right')
+                for sequence in overwritten_sequences:
+                    ax.plot(df['tb_x'].to_numpy()[sequence], values[sequence], c=c, alpha=0.4)
+                ax.plot(df['tb_x'].to_numpy()[kept_rows], values[kept_rows], c=c, label=f"{split}_{loss}")
 
-    if args.save:
-        save_path = os.path.join(cfg.log_dir, f"{'_'.join(args.split)}_losses.png")
-        print(f"saving loss graph under:\n{save_path}")
-        plt.savefig(save_path, bbox_inches='tight', transparent=True, dpi=100)
+            ax.set_xticks(new_epochs_tb_x, minor=True)
+            ax.xaxis.grid(True, which='minor')
+            fig.set_size_inches(16, 9)
+            plt.legend(loc='upper right')
 
-        save_dir = os.path.join(REPO_ROOT, 'results', 'performance_analysis', 'loss_graphs', args.cfg)
-        os.makedirs(save_dir, exist_ok=True)
-        save_path = os.path.join(save_dir, f"{'_'.join(args.split)}_losses.png")
-        print(f"saving loss graph under:\n{save_path}")
-        plt.savefig(save_path, bbox_inches='tight', transparent=True, dpi=100)
-    if args.show:
-        plt.show()
-    print("Goodbye!")
+        if args.save:
+            save_path = os.path.join(cfg.log_dir, f"{'_'.join(args.split)}_losses.png")
+            print(f"saving loss graph under:\n{save_path}")
+            plt.savefig(save_path, bbox_inches='tight', transparent=True, dpi=100)
+
+            save_dir = os.path.join(REPO_ROOT, 'results', 'performance_analysis', 'loss_graphs', cfg_str)
+            os.makedirs(save_dir, exist_ok=True)
+            save_path = os.path.join(save_dir, f"{'_'.join(args.split)}_losses.png")
+            print(f"saving loss graph under:\n{save_path}")
+            plt.savefig(save_path, bbox_inches='tight', transparent=True, dpi=100)
+        if args.show:
+            plt.show()
+        print("Goodbye!")
