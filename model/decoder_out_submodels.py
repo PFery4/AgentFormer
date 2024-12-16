7a9240b526241724dd6a40e17581dc99b0eac01b
Paul Fery <paulfery4@gmail.com>
2024-12-16 13:09:37 +0100

    remove __main__ function (this is not a script)

--- model/decoder_out_submodels.py
+++ model/decoder_out_submodels.py
@@ -18,30 +18,3 @@ def gauss_out_module(model_dim: int, forecast_dim: int, **kwargs) -> nn.Sequenti
     hidden_dims = kwargs.get("hidden_dims", None)
     assert hidden_dims is not None
     return nn.Sequential(GaussianDensityModel(input_dim=model_dim, hidden_dims=hidden_dims, forecast_dim=forecast_dim))
-
-
-if __name__ == '__main__':
-    import torch
-
-    print("Hello")
-    module = point_out_module(256, 2, hidden_dims=[128, 128])
-    print(module)
-    x = torch.randn(256)
-    y = module(x)
-    print(x.shape)
-    print(y.shape)
-
-    module_2 = point_out_module(256, 2)
-    print(module_2)
-    x = torch.randn(256)
-    y = module_2(x)
-    print(x.shape)
-    print(y.shape)
-
-    module_3 = gauss_out_module(256, 2, hidden_dims=[128, 64])
-    print(module_3)
-    x = torch.randn(256)
-    y = module_3(x)
-    print(x.shape)
-    print(y.shape)
-
