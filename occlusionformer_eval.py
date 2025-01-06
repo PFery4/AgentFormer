2cd0e0fb8525dbfd718bb030937a8dd56d536ba4
Paul Fery <paulfery4@gmail.com>
2025-01-06 17:26:33 +0100

    proper specifications of script options and arguments, proper extraction of config file fields, proper instantiation of dataset class

--- occlusionformer_eval.py
+++ occlusionformer_eval.py
@@ -7,8 +7,8 @@ import pandas as pd
 import torch
 from torch.utils.data import DataLoader
 
-from utils.config import Config, REPO_ROOT
-from utils.utils import prepare_seed, print_log, mkdir_if_missing, get_cuda_device
+from utils.config import Config, ModelConfig, REPO_ROOT
+from utils.utils import prepare_seed, print_log, get_cuda_device
 from utils.performance_metrics import \
     compute_samples_ADE,\
     compute_samples_FDE,\
@@ -23,21 +23,18 @@ from model.model_lib import model_dict
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', default=None)
-    parser.add_argument('--dataset_cfg', default=None)
+    parser.add_argument('--cfg', type=str, required=True, default=None,
+                        help="Model config file (specified as either name or path")
+    parser.add_argument('--dataset_cfg', type=str, default=None,
+                        help="Dataset config file (specified as either name or path")
     parser.add_argument('--data_split', type=str, default='test')
     parser.add_argument('--checkpoint_name', default='best_val')        # can be 'best_val' / 'untrained' / <model_id>
     parser.add_argument('--tmp', action='store_true', default=False)
-    parser.add_argument('--gpu', default=None)
-    parser.add_argument('--dataset_class', default='hdf5')          # [hdf5, pickle, torch_preprocess]
+    parser.add_argument('--gpu', type=int, default=None)
+    parser.add_argument('--dataset_class', type=str, default='hdf5', help="'torch' | 'hdf5'")
     args = parser.parse_args()
 
-    dataset_cfg = args.dataset_cfg
-    split = args.data_split
-    checkpoint_name = args.checkpoint_name
-    args.gpu = int(args.gpu) if args.gpu is not None else args.gpu
-
-    cfg = Config(cfg_id=args.cfg, tmp=args.tmp, create_dirs=False)
+    cfg = ModelConfig(cfg_id=args.cfg, tmp=args.tmp, create_dirs=False)
     prepare_seed(cfg.seed)
     torch.set_default_dtype(torch.float32)
 
@@ -48,28 +45,31 @@ if __name__ == '__main__':
     log = open(os.path.join(cfg.log_dir, 'log_test.txt'), 'w')
 
     # dataloader
-    if dataset_cfg is not None:
-        dataset_cfg = Config(cfg_id=dataset_cfg, tmp=False, create_dirs=False)
-    else:
-        dataset_cfg = cfg
-
-    assert dataset_cfg.dataset == 'sdd'
     dataset_class = dataset_dict[args.dataset_class]
+    if args.dataset_cfg is None:
+        args.dataset_cfg = cfg.dataset_cfg
+    dataset_cfg = Config(cfg_id=args.dataset_cfg)
+    dataset_cfg.__setattr__('with_rgb_map', False)
+    assert dataset_cfg.dataset == 'sdd'
     if dataset_cfg.dataset == 'sdd':
-        sdd_test_set = dataset_class(parser=dataset_cfg, log=log, split=split)
+        sdd_test_set = dataset_class(parser=dataset_cfg, split=args.data_split)
         test_loader = DataLoader(dataset=sdd_test_set, shuffle=False, num_workers=0)
     else:
         raise NotImplementedError
 
     # model
     model_id = cfg.get('model_id', 'agentformer')
+    for key in ['future_frames', 'motion_dim', 'forecast_dim', 'global_map_resolution']:
+        assert key in dataset_cfg.yml_dict.keys()
+        cfg.yml_dict[key] = dataset_cfg.__getattribute__(key)
+
     model = model_dict[model_id](cfg)
     model.set_device(device)
     model.eval()
     if model_id in ['const_velocity', 'oracle']:
         checkpoint_name = 'untrained'
     else:
-        if checkpoint_name == 'best_val':
+        if args.checkpoint_name == 'best_val':
             checkpoint_name = cfg.get_best_val_checkpoint_name()
             print(f"Best validation checkpoint name is: {checkpoint_name}")
         cp_path = cfg.model_path % checkpoint_name
@@ -79,7 +79,7 @@ if __name__ == '__main__':
         model.load_state_dict(model_cp['model_dict'])
 
     # loading model predictions
-    saved_preds_dir = os.path.join(cfg.result_dir, sdd_test_set.dataset_name, checkpoint_name, split)
+    saved_preds_dir = os.path.join(cfg.result_dir, sdd_test_set.dataset_name, checkpoint_name, args.data_split)
     assert os.path.exists(saved_preds_dir)
     log_str = f'loading predictions from the following directory:\n{saved_preds_dir}\n\n'
     print_log(log_str, log=log)
