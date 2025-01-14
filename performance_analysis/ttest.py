eefd4fd039195eaaf563413d40ab61b25e65bb8c
Paul Fery <paulfery4@gmail.com>
2025-01-14 22:05:29 +0100

    make the script work with the --score_files argument (following the same philosophy as other performance analysis scripts).

--- performance_analysis/ttest.py
+++ performance_analysis/ttest.py
@@ -1,37 +1,24 @@
 import argparse
 import numpy as np
+import os.path
 import pandas as pd
 import scipy.stats
 
 from utils.performance_analysis import \
+    SCORES_CSV_FILENAME, \
+    MIN_SCORES, MEAN_SCORES, PAST_MIN_SCORES, PAST_MEAN_SCORES, \
     get_reference_indices, \
-    get_all_results_directories, \
     get_df_filter, \
-    get_perf_scores_df
+    get_perf_scores_df, \
+    process_analysis_subjects_txt, \
+    get_experiment_dict, \
+    get_df_from_csv
 
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 200)
 pd.set_option('display.max_colwidth', 75)
-
-DEFAULT_CFG = [
-    'original_100',
-    'original_101',
-    'original_102',
-    'original_103',
-    'original_104',
-    'occlusionformer_basis_bias_1',
-    'occlusionformer_basis_bias_2',
-    'occlusionformer_basis_bias_3',
-    'occlusionformer_basis_bias_4',
-    'occlusionformer_basis_bias_5',
-]
-
-MIN_SCORES = ['min_ADE', 'min_FDE']
-MEAN_SCORES = ['mean_ADE', 'mean_FDE']
-PAST_MIN_SCORES = ['min_past_ADE', 'min_past_FDE']
-PAST_MEAN_SCORES = ['mean_past_ADE', 'mean_past_FDE']
 
 PRED_LENGTHS = ['past_pred_length', 'pred_length']
 OCCLUSION_MAP_SCORES = ['OAO', 'OAC', 'OAC_t0']
@@ -63,6 +50,9 @@ def perform_ttest(array_a: np.array, array_b: np.array, alpha=0.05):
 
 
 def main(args: argparse.Namespace):
+    assert args.unit in ['m', 'px']
+    print("T-TESTS:\n\n")
+
     if args.unit == 'm':
         min_scores, mean_scores = MIN_SCORES, MEAN_SCORES
         past_min_scores, past_mean_scores = PAST_MIN_SCORES, PAST_MEAN_SCORES
@@ -73,20 +63,19 @@ def main(args: argparse.Namespace):
     else:
         raise NotImplementedError
 
-    print("T-TESTS:\n\n")
-    assert args.cfg is not None
-    experiment_names = args.cfg
+    if len(args.score_files) == 1 and args.score_files[0].endswith('.txt'):
+        subjects = process_analysis_subjects_txt(txt_file=args.score_files[0])
+    else:
+        subjects = [file for file in args.score_files if (not file.startswith('#') and file.endswith(SCORES_CSV_FILENAME))]
 
-    exp_dicts = get_all_results_directories()
-    exp_dicts = [exp_dict for exp_dict in exp_dicts if exp_dict['split'] in ['test']]
-    exp_dicts = [exp_dict for exp_dict in exp_dicts if exp_dict['experiment_name'] in experiment_names]
-    # exp_dicts = [exp_dict for exp_dict in exp_dicts if exp_dict['dataset_used'] in ['fully_observed']]
+    for file in subjects:
+        assert os.path.exists(file), f"Error, file does not exist:\n{file}"
 
     test_scores = min_scores + mean_scores
 
     ref_index = get_reference_indices()
     ref_past_pred_lengths = get_perf_scores_df(
-        experiment_name='const_vel_occlusion_simulation',
+        experiment_name='CV_predictor',
         dataset_used='occlusion_simulation',
         model_name='untrained',
         split='test'
@@ -110,19 +99,11 @@ def main(args: argparse.Namespace):
 
         out_df = pd.DataFrame(columns=out_df_columns)
 
-        for exp_dict in exp_dicts:
+        for exp_csv_path in subjects:
 
-            exp_name = exp_dict['experiment_name']
-            dataset_used = exp_dict['dataset_used']
-            model_name = exp_dict['model_name']
-            split = exp_dict['split']
+            exp_dict = get_experiment_dict(exp_csv_path)
+            experiment_df = get_df_from_csv(exp_csv_path)
 
-            experiment_df = get_perf_scores_df(
-                experiment_name=exp_name,
-                dataset_used=dataset_used,
-                model_name=model_name,
-                split=split
-            )
             if df_filter is not None:
                 experiment_df = df_filter(experiment_df)
 
@@ -136,22 +117,11 @@ def main(args: argparse.Namespace):
                 scores_1 = scores_1.values
                 scores_2 = scores_2.values
 
-                # import numpy as np
-                # example A:
-                # scores_1 = np.array([27.5, 21.0, 19.0, 23.6, 17.0, 17.9, 16.9, 20.1, 21.9, 22.6, 23.1, 19.6, 19.0, 21.7, 21.4])
-                # scores_2 = np.array([27.1, 22.0, 20.8, 23.4, 23.4, 23.5, 25.8, 22.0, 24.8, 20.2, 21.9, 22.1, 22.9, 20.5, 24.4])
-                # example B:
-                # scores_1 = np.array([17.2, 20.9, 22.6, 18.1, 21.7, 21.4, 23.5, 24.2, 14.7, 21.8])
-                # scores_2 = np.array([21.5, 22.8, 21.0, 23.0, 21.6, 23.6, 22.5, 20.7, 23.4, 21.8, 20.7, 21.7, 21.5, 22.5, 23.6, 21.5, 22.5, 23.5, 21.5, 21.8])
-                # example C:
-                # scores_1 = np.array([19.8, 20.4, 19.6, 17.8, 18.5, 18.9, 18.3, 18.9, 19.5, 22.0])
-                # scores_2 = np.array([28.2, 26.6, 20.1, 23.3, 25.2, 22.1, 17.7, 27.6, 20.6, 13.7, 23.2, 17.5, 20.6, 18.0, 23.9, 21.6, 24.3, 20.4, 24.0, 13.2])
-
                 is_significant, t_stat, p_val, df, t_crit = perform_ttest(array_a=scores_1, array_b=scores_2)
 
                 row_dict = {
-                    'experiment': exp_name,
-                    'dataset_used': dataset_used,
+                    'experiment': exp_dict['experiment_name'],
+                    'dataset_used': exp_dict['dataset_used'],
                     'n_1': len(scores_1),
                     'mean_1': scores_1.mean(),
                     's^2_1': scores_1.std(ddof=1)**2,
@@ -179,29 +149,23 @@ def main(args: argparse.Namespace):
 
     else:
 
-        # comparisons = [(1, cat_2) for cat_2 in range(2, 6)]
         comparisons = [(0, cat_2) for cat_2 in range(1, 7)]
         def comp_name(comp): return f"-{comp[0]}/-{comp[1]}"
 
-        row_index_tuples = [(exp_dict['experiment_name'], exp_dict['dataset_used']) for exp_dict in exp_dicts]
+        row_index_tuples = []
+        for exp_csv_path in subjects:
+            exp_dict = get_experiment_dict(exp_csv_path)
+            row_index_tuples.append((exp_dict['experiment_name'], exp_dict['dataset_used']))
         row_df_index = pd.MultiIndex.from_tuples(row_index_tuples, names=['experiment', 'dataset_used'])
         col_index_tuples = [(test_score, comp_name(comp)) for test_score in test_scores for comp in comparisons]
         col_df_index = pd.MultiIndex.from_tuples(col_index_tuples, names=['test_score', 'categories'])
 
         out_df = pd.DataFrame(index=row_df_index, columns=col_df_index)
 
-        for exp_dict in exp_dicts:
-            exp_name = exp_dict['experiment_name']
-            dataset_used = exp_dict['dataset_used']
-            model_name = exp_dict['model_name']
-            split = exp_dict['split']
+        for exp_csv_path in subjects:
+            exp_dict = get_experiment_dict(exp_csv_path)
+            experiment_df = get_df_from_csv(exp_csv_path)
 
-            experiment_df = get_perf_scores_df(
-                experiment_name=exp_name,
-                dataset_used=dataset_used,
-                model_name=model_name,
-                split=split
-            )
             if df_filter is not None:
                 experiment_df = df_filter(experiment_df)
 
@@ -223,7 +187,9 @@ def main(args: argparse.Namespace):
 
                     is_significant, t_stat, p_val, df, t_crit = perform_ttest(array_a=scores_1, array_b=scores_2)
 
-                    out_df.loc[(exp_name, dataset_used), (test_score, comp_name(comp))] = is_significant
+                    out_df.loc[
+                        (exp_dict['experiment_name'], exp_dict['dataset_used']), (test_score, comp_name(comp))
+                    ] = is_significant
 
         print(out_df)
 
@@ -231,11 +197,18 @@ def main(args: argparse.Namespace):
 if __name__ == '__main__':
     print("Hello!")
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', nargs='+', default=None)
-    parser.add_argument('--filter', nargs='+', default=None)
-    parser.add_argument('--comp_tlast', nargs=2,
-                        help='specify 2 last observed timestep categories to compare', type=int, default=None)
-    parser.add_argument('--unit', type=str, default='m')        # 'm' | 'px'
+    parser.add_argument('--score_files', nargs='+', type=os.path.abspath, required=True,
+                        help="provide either multiple paths to 'prediction_scores.csv' files inside the 'results' "
+                             "directory, or a path to a single .txt file, containing paths to those files "
+                             "(relative to the repository's root directory).")
+    parser.add_argument('--unit', type=str, default='m',
+                        help="\'m\' | \'px\'")
+    parser.add_argument('--filter', nargs='+', type=str, default=None,
+                        help="select any number of options from:\n"
+                             "\'occluded_ids\', \'fully_observed_ids\', \'difficult_dataset\', "
+                             "\'difficult_occluded_ids\', \'moving\', \'idle\'")
+    parser.add_argument('--comp_tlast', nargs=2, type=int, default=None,
+                        help='specify 2 last observed timestep categories to compare')
     args = parser.parse_args()
 
     main(args=args)
