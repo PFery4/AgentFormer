75574f2ce4d22d690a1e7cbf8bea2511d08828ad
Paul Fery <paulfery4@gmail.com>
2025-01-14 19:19:14 +0100

    rework script to operate with a --score_file argument, (similar to how other performance analysis scripts operate).

--- performance_analysis/occlusion_score_histograms.py
+++ performance_analysis/occlusion_score_histograms.py
@@ -2,12 +2,17 @@ import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import os.path
 
 import matplotlib.figure
 from typing import Any, Optional, Tuple
 
 from utils.performance_analysis import \
-    get_perf_scores_df
+    SCORES_CSV_FILENAME, \
+    get_all_pred_scores_csv_files, \
+    process_analysis_subjects_txt, \
+    get_experiment_dict, \
+    get_df_from_csv
 
 
 FIG_SIZE = (16, 10)
@@ -17,14 +22,14 @@ AS_PERCENTAGE = False
 
 def oac_histogram(
         draw_ax: matplotlib.axes.Axes,
-        experiment_name: str,
+        exp_csv_path: str,
         plot_score: str,
         categorization: Tuple[str, Any],
         as_percentage: Optional[bool] = False,
         n_bins: int = 20
 ):
     # categorization [0, 1] <--> [column in experiment_df, value by which to filter that column]
-    experiment_df = get_perf_scores_df(experiment_name)
+    experiment_df = get_df_from_csv(exp_csv_path)
     mini_df = experiment_df[
         (experiment_df[categorization[0]] == categorization[1]) & (pd.notna(experiment_df[plot_score]))
         ]
@@ -34,16 +39,13 @@ def oac_histogram(
 
     draw_ax.hist(scores, bins=n_bins, weights=weights)
 
-    # draw_ax.set_xlabel(f"{plot_score}", loc='left')
-    # draw_ax.set_title(f"{plot_score} histogram: {experiment_name} (last observed timestep: {-categorization[1]})")
-
 
 def oac_histograms_versus_lastobs(
         draw_ax: matplotlib.axes.Axes,
-        experiment_name: str,
+        exp_csv_path: str,
         plot_score: str,
 ):
-    experiment_df = get_perf_scores_df(experiment_name)
+    experiment_df = get_df_from_csv(exp_csv_path)
 
     mini_df = experiment_df[pd.notna(experiment_df[plot_score])]
 
@@ -51,7 +53,6 @@ def oac_histograms_versus_lastobs(
     past_pred_lengths = mini_df['past_pred_length'].to_numpy()
     unique, counts = np.unique(past_pred_lengths, return_counts=True)
     weights = 1 / counts[past_pred_lengths - 1]
-    # weights = 1 / (counts[occl_lengths-1] * unique.shape[0])
 
     hist = draw_ax.hist2d(
         scores1, past_pred_lengths,
@@ -62,12 +63,11 @@ def oac_histograms_versus_lastobs(
     draw_ax.get_figure().colorbar(hist[3], ax=draw_ax)
     draw_ax.set_xlabel(plot_score)
     draw_ax.set_ylabel("last observed timestep")
-    # draw_ax.set_title(f"OAC histogram: {experiment_name}")
 
 
 def make_oac_histograms_figure(
         fig: matplotlib.figure.Figure,
-        experiment_name: str,
+        exp_csv_path: str,
         plot_score: str,
         as_percentage: bool = False
 ):
@@ -79,34 +79,32 @@ def make_oac_histograms_figure(
     ax_twodee = fig.add_subplot(gs[:, 1])
 
     for i, ax in enumerate(reversed(ax_list)):
-        oac_histogram(draw_ax=ax, experiment_name=experiment_name, plot_score=plot_score,
+        oac_histogram(draw_ax=ax, exp_csv_path=exp_csv_path, plot_score=plot_score,
                       categorization=('past_pred_length', i + 1), as_percentage=as_percentage)
-    oac_histograms_versus_lastobs(draw_ax=ax_twodee, experiment_name=experiment_name, plot_score=plot_score)
+    oac_histograms_versus_lastobs(draw_ax=ax_twodee, exp_csv_path=exp_csv_path, plot_score=plot_score)
     ax_list[-1].set_xlabel(plot_score)
-    fig.suptitle(f"{plot_score} histograms: {experiment_name}")
+    fig.suptitle(f"{plot_score} histograms: {get_experiment_dict(exp_csv_path)['experiment_name']}")
 
 
 def main(args: argparse.Namespace):
     print(f"{PLOT_SCORE} HISTOGRAMS:\n\n")
 
-    experiment_names = args.cfg
+    assert args.score_file.endswith(SCORES_CSV_FILENAME)
+    assert os.path.exists(args.score_file), f"Error, file does not exist:\n{args.score_file}"
 
-    for experiment_name in experiment_names:
-        fig = plt.figure(figsize=FIG_SIZE)
-        make_oac_histograms_figure(
-            fig=fig, experiment_name=experiment_name, plot_score=PLOT_SCORE, as_percentage=AS_PERCENTAGE
-        )
+    fig = plt.figure(figsize=FIG_SIZE)
+    make_oac_histograms_figure(
+        fig=fig, exp_csv_path=args.score_file, plot_score=PLOT_SCORE, as_percentage=AS_PERCENTAGE
+    )
 
     plt.show()
-
-    # if SAVE:
-    #     print("OAC_histograms: no saving implementation (yet)!")
 
 
 if __name__ == '__main__':
     print("Hello!")
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', nargs='+', default=None)
+    parser.add_argument('--score_file', type=os.path.abspath, required=True,
+                        help="provide a path to a 'prediction_scores.csv' file inside the 'results' directory.")
     args = parser.parse_args()
 
     main(args=args)
